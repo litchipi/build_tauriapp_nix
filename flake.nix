c955a3cb03a18acda7d0115eaff8e04250f4e075
Litchi Pi <litchi.pi@protonmail.com>
2022-10-01 12:02:23 +0200

    dev
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -39,6 +39,79 @@
       );
     };
 
+    default_rust_toolchain = pkgs.rust-bin.fromRustupToolchain {
+      channel = "stable";
+      targets = let 
+        llvmTriple = with lib.systems.parse; tripleFromSystem (mkSystemFromString system);
+      in [ llvmTriple ];
+      components = [];
+    };
+
+    rustPkgs = {
+      cargo2nix_file,
+      rustVersion,
+      target ? null,
+      rootFeatures ? [],
+      rustToolchain ? default_rust_toolchain,
+      add_overrides ? [],
+    ... }: pkgs.rustBuilder.makePackageSet {
+      inherit target rootFeatures rustVersion;
+      fetchCrateAlternativeRegistry = pkgs.rustBuilder.rustLib.fetchCrateAlternativeRegistryExpensive;
+      packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ add_overrides;
+      packageFun = import cargo2nix_file;
+    };
+
+    debugBuildDep = name: deps: pkgs.rustBuilder.rustLib.makeOverride {
+      inherit name;
+      overrideAttrs = drv: {
+        # propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ deps;
+        propagatedNativeBuildInputs = drv.propagatedNativeBuildInputs or [] ++ deps;
+      };
+    };
+
+    addBuildDep = name: deps: pkgs.rustBuilder.rustLib.makeOverride {
+      inherit name;
+      overrideAttrs = drv: { propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ deps; };
+    };
+
+    build_tauri_backend = cfg: ((rustPkgs (cfg.backend // {
+      add_overrides = let
+        builddepsall = with pkgs; [
+          pkg-config
+          # gtk3.dev
+          gtk3-x11.dev
+          wayland.bin
+          wayland.dev
+          wayland-utils
+          wayland-protocols
+          dbus
+          webkitgtk
+          cmake
+          libsoup
+          indicator-application-gtk3
+          libayatana-indicator-gtk3
+          glib.dev
+          cairo.dev
+          atk.dev
+          gdk-pixbuf.dev
+          webkitgtk.dev
+          freetype
+        ];
+      in with pkgs; [
+        (addBuildDep "glib-sys" [ glib.dev ])
+        (addBuildDep "cairo-sys-rs" [ cairo.dev ])
+        (addBuildDep "atk-sys" [ atk.dev ])
+        (addBuildDep "gdk-pixbuf-sys" [ gdk-pixbuf.dev ])
+        (addBuildDep "javascriptcore-rs-sys" [ webkitgtk.dev ])
+        (addBuildDep "pango-sys" [ pango.dev ])
+        (addBuildDep "soup2-sys" [ libsoup.dev ])
+
+        (debugBuildDep "gdk-sys" [ pkg-config gtk3.dev ])
+        (debugBuildDep "gdk" [ pkg-config gtk3.dev ])
+        (debugBuildDep "gdkx11-sys" [ pkg-config gtk3.dev ])
+      ];
+    })).workspace.${cfg.name} { }).bin;
+
     build_tauri_frontend = cfg: import ./build_frontend.nix {
       name = "${cfg.name}_frontend";
       src = cfg.src;
@@ -62,6 +135,7 @@
 
       postPatch = ''
         cp ${generate_tauri_config cfg} ./tauri.conf.json
+        ls ${build_tauri_backend cfg}/
       '';
     };
 
@@ -79,6 +153,7 @@
         rustVersion = "1.63.0";
         rustChannel = "stable";
         src = ./test/src-tauri;
+        cargo2nix_file = ./test/src-tauri/Cargo.nix;
       };
     };
   });
